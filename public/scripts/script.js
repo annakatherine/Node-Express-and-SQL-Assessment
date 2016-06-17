c279e933ce3bd69a6d9d488230b71852b0ba2a92
mablchnole <mablchnole@gmail.com>
2016-06-17 16:46:04 -0500

    app is functioning

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,19 +1,43 @@
-console.log('in script.js');
 
 $(document).ready(function(){
-  console.log('in jquery.min.js');
-  $('#testBtn').on('click', function(){
-    console.log('testBtn clicked');
-    // assemble ann object (always for a post call)
-    var objectToSend = {
-      username: "username"
-    }; // end objectToSend
 
-    // send object to postRoute via ajax
+  // event listener for add button
+  $('#addBtn').on('click', function(){
+    // get new animal from input
+    var animalType = $('#animalIn').val();
+    var animalCount = randomNumber(1,100);
+    // create new object to post
+    var animalToSend = {
+      "type": animalType,
+      "count": animalCount
+    }; // end object animalToSend
+    // send object to server as a post
     $.ajax({
       type: 'post',
-      url: '/postRoute',
-      data: objectToSend
+      url: '/addAnimal',
+      data: animalToSend
     }); // end ajax
-  }); // end testBtn
-});
+    location.reload();
+  }); // end addBtn
+
+  // receive data of all animals from db
+  $.ajax({
+    type: 'GET',
+    url: '/getAnimals',
+    success: function(data){
+    showAnimals(data);
+    } // end success
+  }); //end ajax
+
+  // display all animals in dom when app loads
+  function showAnimals(animals){
+    for(i=0; i<animals.length; i++){
+      var animalRow = '<td>' + animals[i].id + '</td>' + '<td>' + animals[i].type + '</td>' + '<td>' + animals[i].count + '</td>';
+      document.getElementById('displayAnimals').innerHTML += '<tr>' + animalRow + '<tr>';
+    } // end for loop
+  } // end showAnimals
+}); // end jquery
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ var app=express();
 var path=require('path');
 var bodyParser=require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
-var pg = require('pg');
-var connectionString='postgres://localhost:5432/testDB';
+var pg=require('pg');
+var connectionString='postgres://localhost:5432/zoo';
 
 // spin up server
-app.listen(8080, 'localhost', function(req, res){
-  console.log('8080 is listening');
+app.listen(3000, 'localhost', function(req, res){
+  console.log('3000 is listening');
 });
 
 // static route for public folder
@@ -18,9 +18,34 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
   console.log('in base url');
   res.sendFile(path.resolve('views/index.html'));
-});
+}); // end base url
 
-// post route (requires urlencodedParser injection)
-app.post('/postRoute', urlencodedParser, function(req, res){
-  console.log('in postRoute, trying to post to db: ' + req.body.username);
-});
+// creates a new animal from the req.body object that is received
+app.post('/addAnimal', urlencodedParser, function(req, res){
+  console.log('in addAnimal: ' + req.body.type + " " + req.body.count);
+  pg.connect( connectionString, function( err, client, done ){
+    client.query('INSERT INTO animals (type, count) VAlUES ($1, $2)', [req.body.type, req.body.count]);
+  });
+}); // end addAnimal
+
+// send back all records in animals
+app.get( '/getAnimals', function( req, res ){
+  console.log( 'in get animals' );
+// this wil hold our results
+  var results =[];
+  pg.connect( connectionString, function( err, client, done ){
+    // get all user records and store in "query" variable
+    var callDB = client.query( 'SELECT * FROM animals;' );
+    console.log( "query: " + callDB );
+    // push each row in query into our results array
+    callDB.on( 'row', function ( row ){
+      results.push( row );
+    }); // end query push
+    callDB.on( 'end', function (){
+      return res.json( results );
+    }); // end onEnd
+    if(err){
+      console.log(err);
+    } // end error
+  }); // end connect
+}); // end animals get
